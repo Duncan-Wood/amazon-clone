283e2f749c35a3ece2dd8459ea98dff01b8633f2
Duncan <duncanwoodpro@gmail.com>
2023-06-23 16:31:44 -0400

    started amazon clone project

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvYmvdUDWehzbdSft5pwsw54mmkuf-0oU",
+  authDomain: "clone-153b6.firebaseapp.com",
+  projectId: "clone-153b6",
+  storageBucket: "clone-153b6.appspot.com",
+  messagingSenderId: "860432028386",
+  appId: "1:860432028386:web:f467df2d65d6c0d30d04bb",
+  measurementId: "G-M133C4NK23",
+};
